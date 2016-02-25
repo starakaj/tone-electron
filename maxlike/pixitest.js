84cc41f2a7cbd144f44f6f924bf52c5f598ba2b3
Sam Tarakajian <starakajian@gmail.com>
2016-02-25 15:16:07 -0500

    Look, you can draw lines now. That's pretty cool, right?

--- maxlike/pixitest.js
+++ maxlike/pixitest.js
@@ -1,39 +1,52 @@
-var renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0x1099bb});
+var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight,{backgroundColor : 0x1099bb, antialias:true});
 document.body.appendChild(renderer.view);
+
+var patchcord = null;
+var cordStartPoint = null;
+
+window.onresize = function(event) {
+  console.log("Resizing");
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  renderer.view.style.width = w + "px";
+  renderer.view.style.height = h + "px";
+  renderer.resize(w, h);
+  stage.hitArea = new PIXI.Rectangle(0, 0, renderer.width, renderer.height);
+}
 
 // create the root of the scene graph
 var stage = new PIXI.Container();
 stage.hitArea = new PIXI.Rectangle(0, 0, renderer.width, renderer.height);
-
-// create a texture from an image path
-var texture = PIXI.Texture.fromImage('assets/basics/bunny.png');
-
-// create a new Sprite using the texture
-var bunny = new PIXI.Sprite(texture);
-
-// center the sprite's anchor point
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
-
-// move the sprite to the center of the screen
-bunny.position.x = 200;
-bunny.position.y = 150;
-
-stage.addChild(bunny);
-
 stage.interactive = true;
-stage.on("mousedown", function(event) {
-  bunny.position = new PIXI.Point(event.data.global.x, event.data.global.y);
-});
+stage.on("mousedown", beginPatchCord);
+stage.on("mousemove", continuePatchCord);
+stage.on("mouseup", endPatchCord);
+stage.on("mouseupoutside", endPatchCord);
+
+function beginPatchCord(event) {
+  patchcord = new PIXI.Graphics();
+  cordStartPoint = new PIXI.Point(event.data.global.x, event.data.global.y);
+  stage.addChild(patchcord);
+}
+
+function continuePatchCord(event) {
+  if (patchcord) {
+    patchcord.clear();
+    patchcord.lineStyle(4, 0x110000, 1)
+    patchcord.moveTo(cordStartPoint.x, cordStartPoint.y);
+    patchcord.lineTo(event.data.global.x, event.data.global.y);
+  }
+}
+
+function endPatchCord(event) {
+  patchcord.clear();
+  stage.removeChild(patchcord);
+  patchcord = null;
+}
 
 // start animating
 animate();
 function animate() {
     requestAnimationFrame(animate);
-
-    // just for fun, let's rotate mr rabbit a little
-    bunny.rotation += 0.1;
-
-    // render the container
     renderer.render(stage);
 }
