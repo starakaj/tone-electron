94780784c36c3d24a2f7c6cc71a0b17546e8965f
Sam Tarakajian <starakajian@gmail.com>
2016-02-25 09:49:20 -0500

    Okay, nothings working yet, but I'm trying to understand mouse events and position

--- maxlike/soundcheck.js
+++ maxlike/soundcheck.js
@@ -6,6 +6,8 @@ var synth = new Tone.SimpleSynth().toMaster();
 var gl;
 var THREE = require("three");
 var scene, renderer, camera, cube;
+var cubes = [];
+var mouse_vector = new THREE.Vector3();
 
 function playArpeggio() {
   synth.triggerAttackRelease("A2", "8n",  "+4n * 0");
@@ -29,21 +31,49 @@ function initCubeScene() {
   var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
   cube = new THREE.Mesh( geometry, material );
   scene.add( cube );
+  cubes.push(cube);
 
   camera.position.z = 5;
 
   window.addEventListener('resize', function() {
     renderer.setSize(window.innerWidth, window.innerHeight);
   }, false);
+
+  renderer.domElement.addEventListener('mousedown', handleClick);
 }
 
 function render() {
 	requestAnimationFrame( render );
 
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
+  for (var i=0; i<cubes.length; i++) {
+    var cb = cubes[i];
+    cb.rotation.x += 0.1;
+    cb.rotation.y += 0.1;
+  }
 
 	renderer.render( scene, camera );
+}
+
+function addCube(vec) {
+  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+  var newcube = new THREE.Mesh(geometry, material);
+  newcube.position.set(vec.x, vec.y, vec.z);
+  newcube.__dirtyPosition = true;
+
+  scene.add(newcube);
+  cubes.push(newcube);
+}
+
+function handleClick(e) {
+  e.preventDefault();
+  var x = (e.clientX / window.innerWidth) * 2 - 1;
+  var y = -(e.clientY / window.innerHeight) * 2 + 1;
+  mouse_vector.set(x, y, 1);
+  mouse_vector.project(camera);
+  console.log(mouse_vector);
+
+  addCube(mouse_vector);
 }
 
 $(document).ready(function() {
