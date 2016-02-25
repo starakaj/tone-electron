3baaccdc43233f8680cd15cf1c99c8026246a0ed
Sam Tarakajian <starakajian@gmail.com>
2016-02-25 09:19:30 -0500

    Well it doesn't look much like Max right now, but I have got an OpenGL context and a rotating cube

--- maxlike/soundcheck.js
+++ maxlike/soundcheck.js
@@ -3,6 +3,9 @@
 var $ = require("jquery");
 var Tone = require("Tone");
 var synth = new Tone.SimpleSynth().toMaster();
+var gl;
+var THREE = require("three");
+var scene, renderer, camera, cube;
 
 function playArpeggio() {
   synth.triggerAttackRelease("A2", "8n",  "+4n * 0");
@@ -14,6 +17,36 @@ function playArpeggio() {
   synth.triggerAttackRelease("A4", "8n",  "+4n * 6");
 }
 
+function initCubeScene() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+
+  camera.position.z = 5;
+
+  window.addEventListener('resize', function() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }, false);
+}
+
+function render() {
+	requestAnimationFrame( render );
+
+  cube.rotation.x += 0.1;
+  cube.rotation.y += 0.1;
+
+	renderer.render( scene, camera );
+}
+
 $(document).ready(function() {
-  $("button[name=arpeggio]").click(playArpeggio);
+  initCubeScene();
+  render();
 });
